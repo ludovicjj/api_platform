f92ec25de64a901d8e9181565277c8379bc3fcc6
ludovicjj <jahanlud@gmail.com>
2021-05-05 01:56:37 +0200

    Set Get operation for category to null

---
+++ src/OpenApi/OpenApiFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\OpenApi;
+
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\OpenApi;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    private $decorated;
+
+    public function __construct(OpenApiFactoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+
+        $pathItem = $openApi->getPaths()->getPath('/api/categories/{id}');
+        $openApi->getPaths()->addPath('/api/categories/{id}', $pathItem->withGet(null));
+
+        return $openApi;
+    }
+}
