0f3331ff2e39d56d7575fc8dd15bd66d1dac7ad5
ludovicjj <jahanlud@gmail.com>
2021-05-11 04:49:21 +0200

    Login action to login by form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -27,5 +29,24 @@ class SecurityController extends AbstractController
      */
     public function logout() {
 
+    }
+
+    /**
+     * @Route("login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $error
+            ]
+        );
     }
 }
