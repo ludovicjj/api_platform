002aa04eed8c33e6cb98a91e60d214e8124808ec
ludovicjj <jahanlud@gmail.com>
2021-05-11 08:04:35 +0200

    Refactor AuthApiFactory

--- src/OpenApi/AuthApiFactory.php
+++ src/OpenApi/AuthApiFactory.php
@@ -21,75 +21,97 @@ class AuthApiFactory implements OpenApiFactoryInterface
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = $this->decorated->__invoke($context);
-        $schemas = $openApi->getComponents()->getSchemas();
-        $schemas['Credentials'] = new \ArrayObject([
-           'type' => 'object',
-           'properties' => [
-               'username' => [
-                   'type' => 'string',
-                   'example' => 'john@doe.fr'
-               ],
-               'password' => [
-                   'type' => 'string',
-                   'example' => 'secret'
-               ]
-           ]
-        ]);
 
-        // Create new entry POST : /api/login
-        $operation = new Operation(
-            'postLoginApi',
-            ['Authentication'],
-            [],
-            "Login",
-            "Provide credentials to login",
-            null,
-            [],
-            new RequestBody(
-                'Your credentials',
-                new \ArrayObject([
-                    'application/json' => [
-                        'schema' => [
-                            '$ref' => '#/components/schemas/Credentials'
-                        ]
-                    ]
-                ])
-            )
-        );
-        $operation->addResponse(
-            new Response(
-                'authentication successful',
-                new \ArrayObject([
-                    'application/json' => [
-                        'schema' => [
-                            '$ref' => '#/components/schemas/User-read.user'
-                        ]
-                    ]
-                ])
-            ),
-            200
-        );
-        $operation->addResponse(
-            new Response(
-                'authentication fail',
-                new \ArrayObject([
-                    'application/json' => [
-                        'schema' => [
-                            'type' => 'object',
-                            'properties' => [
-                                'error' => [
-                                    'type' => 'string'
+        // Create new path POST : /api/login
+        $loginOperation = new Operation();
+        $loginOperation = $loginOperation
+            ->withOperationId('postLoginApi')
+            ->withTags(['Authentication'])
+            ->withSummary("Login")
+            ->withDescription("Provide credentials to login")
+            ->withRequestBody(
+                new RequestBody(
+                    'Your credentials',
+                    new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'username' => [
+                                        'type' => 'string',
+                                        'example' => 'john@doe.fr'
+                                    ],
+                                    'password' => [
+                                        'type' => 'string',
+                                        'example' => 'secret'
+                                    ]
                                 ]
                             ]
                         ]
-                    ]
-                ])
-            ),
-            401
-        );
+                    ])
+                )
+            )
+            ->addResponse(
+                new Response(
+                    'Authentication successful',
+                    new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'email' => [
+                                        'type' => 'string',
+                                        'example' => 'john@doe.fr'
+                                    ],
+                                    'roles' => [
+                                        'type' => 'array',
+                                        'items' => [
+                                            'type' => 'string'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                ),
+                200
+            )
+            ->addResponse(
+                new Response(
+                    'Authentication fail',
+                    new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'error' => [
+                                        'type' => 'string'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                ),
+                401
+            );
+        $loginPath = new PathItem();
+        $loginPath = $loginPath->withPost($loginOperation);
 
-        $pathItem = new PathItem(null, null, null, null, null, $operation);
-        $openApi->getPaths()->addPath('/api/login', $pathItem);
+
+        // Create new path POST : /api/logout
+        $logoutOperation = new Operation();
+        $logoutOperation = $logoutOperation
+            ->withOperationId('postLogoutApi')
+            ->withTags(['Authentication'])
+            ->withSummary('Logout')
+            ->withDescription('Logout current User')
+            ->addResponse(new Response('Logout success'), 204);
+        $logoutPath = new PathItem();
+        $logoutPath = $logoutPath->withPost($logoutOperation);
+
+
+        $openApi->getPaths()->addPath('/logout', $logoutPath);
+        $openApi->getPaths()->addPath('/api/login', $loginPath);
 
         return $openApi;
     }
