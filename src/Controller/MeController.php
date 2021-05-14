649731cf78842604b826fd58bba92c5de50de515
ludovicjj <jahanlud@gmail.com>
2021-05-14 12:35:12 +0200

    Update MeController

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -3,7 +3,6 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\Security\Core\Security;
 
 class MeController
@@ -11,15 +10,17 @@ class MeController
     /** @var Security $security */
     private $security;
 
-    public function __construct(Security $security)
+    public function __construct(
+        Security $security
+    )
     {
         $this->security = $security;
     }
 
     public function __invoke()
     {
-        $user = $this->security->getUser();
         // let api_plarform serialize it by normalization_context
-        return $user;
+        // User returned by security is the user created into User::createFromPayload
+        return $this->security->getUser();
     }
 }
