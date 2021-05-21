da2a44e5967062c434a3238cdda7d8f9bc4435dc
ludovicjj <jahanlud@gmail.com>
2021-05-21 10:14:09 +0200

    Update field updatedAt to call event listener

--- src/Controller/PostImageController.php
+++ src/Controller/PostImageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\Post;
+use http\Exception\RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 
 class PostImageController
@@ -12,7 +13,16 @@ class PostImageController
     public function __invoke(Request $request)
     {
         $post = $request->attributes->get('data');
-        $file = $request->files->get('file');
-        dd($file, $request);
+
+        if (!$post instanceof Post) {
+            throw new RuntimeException('expected object instance of App\Entity\Post');
+        }
+
+        $post->setFile($request->files->get('file'));
+        // It is required that at least one field changes if you are using doctrine
+        // otherwise the event listeners won't be called and the file is lost
+        $post->setUpdatedAt(new \DateTime());
+
+        return $post;
     }
 }
